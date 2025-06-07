c5b4921b995699c3268db693ee212e76e3cfbf58
LeonardoAlvesw0409 <leonardo.alves.lima@escola.pr.gov.br>
2025-06-07 16:59:41 -0300

    Add files via upload

---
+++ FLASHCARDS/FLASHCARDS/assests/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Biologia',
+    'O que é Biotecnologia Vermelha?',
+    'A biotecnologia vermelha é a área da biotecnologia que se concentra na saúde humana, medicina e diagnóstico, Abrange o desenvolvimento de vacinas, medicamentos, terapias (celulares e gênicas), ferramentas de diagnóstico'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é Biotecnologia Preta?',
+    'A biotecnologia preta é o estudo e desenvolvimento de tecnologias que podem ser usadas para causar danos à saúde humana e ao meio ambiente, como armas biológicas e agentes de bioterrorismo.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é Biotecnologia Roxa?',
+    'A biotecnologia roxa é o estudo das questões éticas, legais e de propriedade intelectual relacionadas à biotecnologia. É uma área que se preocupa com a regulamentação de patentes, publicações, e com a análise de dilemas morais que surgem a partir do uso de organismos vivos na biotecnologia.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é Biotecnologia Azul?',
+    'A biotecnologia azul, ou biotecnologia marinha, é a aplicação de ciência e tecnologia em organismos aquáticos e seus produtos para desenvolver conhecimento, bens e serviços, com foco na exploração sustentável dos recursos marinhos'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é Biotecnologia Marrom?',
+    'A biotecnologia marrom concentra-se no desenvolvimento de soluções para ambientes áridos e desérticos, como a criação de sementes resistentes à seca e o desenvolvimento de técnicas de cultivo adaptadas a estas condições extremas. É um ramo da biotecnologia que busca melhorar a vida humana e a biodiversidade em regiões com pouca água e solos pouco férteis.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é Biotecnologia Cinza?',
+    'A biotecnologia cinza refere-se à aplicação da biotecnologia em áreas ambientais, com foco na preservação, recuperação e proteção do meio ambiente, incluindo processos de biorremediação e reaproveitamento de resíduos.'
+)
